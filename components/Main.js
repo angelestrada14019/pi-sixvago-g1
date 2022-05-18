3164ddbf6eafea7724efcc6ab832b81aa192932a
Gabriel Franke <frankegabriel@gmail.com>
2022-05-18 23:36:09 +0000

    Upload New File

---
+++ components/Main.js
@@ -0,0 +1,12 @@
+import main from "./main.css"
+import Cards from "./Cards.js"
+
+const Main = () => {
+  return (
+      <div className="container">
+          <Cards />
+    </div>
+  )
+}
+
+export default Main
