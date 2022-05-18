52668cca1b03bbdf02b58b8e53e304fd32ff9fc6
Gabriel Franke <frankegabriel@gmail.com>
2022-05-18 23:35:32 +0000

    Upload New File

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import footer from "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-copyright">
+        <h3>©2021 Digital Booking</h3>
+      </div>
+      <div className="footer-icons">
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-linkedin-in"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-instagram"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
