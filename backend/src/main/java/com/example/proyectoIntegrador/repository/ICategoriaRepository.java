075ff894e3f88cbb30c4399cfdc490067eb77595
magaliff <xxmagaaf@gmail.com>
2022-05-19 17:46:15 -0300

    remove folder

--- backend/src/main/java/com/example/proyectoIntegrador/repository/ICategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyectoIntegrador.repository;
-
-import com.example.proyectoIntegrador.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoriaRepository extends JpaRepository<Categoria,Long> {
-}
