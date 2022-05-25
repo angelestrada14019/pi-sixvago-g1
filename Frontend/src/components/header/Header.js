1046e7e698502ebfe3d80f84d65eb3942ecdf27a
GabsFranke <frankegabriel@gmail.com>
2022-05-25 19:00:18 -0300

    Conversion de la bienvenida a un componente

--- Frontend/src/components/header/Header.js
+++ Frontend/src/components/header/Header.js
@@ -4,7 +4,8 @@ import menu from "../../assets/menu.png";
 import { useEffect, useState } from "react";
 import Login from "../login/Login";
 import Register from "../login/Register";
-import Sidebar from "./sidebar"
+import Sidebar from "./sidebar";
+import UserWelcome from "./UserWelcome";
 
 const Header = () => {
   const [toggleNavButton, setToggleNavButton] = useState("");
@@ -20,12 +21,12 @@ const Header = () => {
     }
     setIsLoggedIn(JSON.parse(localStorage.getItem("isLoggedIn")));
   }, [openLogin, openSignUp, isLoggedIn]);
-  
+
   const toggleSidebar = () => {
-    setShowSidebar(!showSidebar)
+    setShowSidebar(!showSidebar);
     console.log(showSidebar);
   };
-  
+
   const handleClick = (e) => {
     if (e.target.id === "crear") {
       setToggleNavButton("crear");
@@ -51,64 +52,51 @@ const Header = () => {
     <>
       <header>
         <div className="logo">
-          <a href=""><img src={logo} alt="logo" /></a>
-          <h1><a href="">Sixvago</a></h1>
+          <a href="">
+            <img src={logo} alt="logo" />
+          </a>
+          <h1>
+            <a href="">Sixvago</a>
+          </h1>
         </div>
         <nav>
           {toggleNavButton === "crear" && !isLoggedIn ? (
             <div>
-              <p className="boton-nav" id="iniciar" onClick={handleClick}>Iniciar sesion</p>
+              <p className="boton-nav" id="iniciar" onClick={handleClick}>
+                Iniciar sesion
+              </p>
             </div>
           ) : toggleNavButton === "iniciar" && !isLoggedIn ? (
             <div>
-              <p className="boton-nav" id="crear" onClick={handleClick}>Crear cuenta</p>
+              <p className="boton-nav" id="crear" onClick={handleClick}>
+                Crear cuenta
+              </p>
             </div>
           ) : !isLoggedIn ? (
             <>
               <div>
-                <p className="boton-nav" id="iniciar" onClick={handleClick}>Iniciar sesion</p>
+                <p className="boton-nav" id="iniciar" onClick={handleClick}>
+                  Iniciar sesion
+                </p>
               </div>
               <div>
-                <p className="boton-nav" id="crear" onClick={handleClick}>Crear cuenta</p>
+                <p className="boton-nav" id="crear" onClick={handleClick}>
+                  Crear cuenta
+                </p>
               </div>
             </>
           ) : !isLoggedIn ? null : (
-            <div className="user-loggedIn-main-container">
-              <div className="user-avatar-container">
-                <h2>
-                  {`
-                ${JSON.parse(localStorage.getItem("user")).username.slice(0, 1)}
-                ${JSON.parse(localStorage.getItem("user")).lastname.slice(0, 1)}
-                `}
-                </h2>
-              </div>
-              <div className="user-welcome-container">
-                <li type="none">
-                  <ul>
-                    <p>Hola, </p>
-                  </ul>
-                  <ul className="user-welcome-username">
-                    <p>{`
-                    ${JSON.parse(localStorage.getItem("user")).username}
-                    ${JSON.parse(localStorage.getItem("user")).lastname}        
-                    `}</p>
-                  </ul>
-                </li>
-                <div className="user-logout-container" onClick={handleLogout}>
-                  <i class="fa-solid fa-xmark"></i>
-                </div>
-              </div>
-            </div>
+            <UserWelcome handleLogout={handleLogout} />
           )}
           <div className="menu-hamburger">
-          <img
-            className="hamburguesa"
-            id="responsive-nav"
-            onClick={toggleSidebar}
-            src={menu}
-            alt="menu"
-          />
-        </div>
+            <img
+              className="hamburguesa"
+              id="responsive-nav"
+              onClick={toggleSidebar}
+              src={menu}
+              alt="menu"
+            />
+          </div>
         </nav>
       </header>
       <Login
@@ -117,7 +105,12 @@ const Header = () => {
         setIsLoggedIn={setIsLoggedIn}
       />
       <Register show={openSignUp} />
-      <Sidebar show={showSidebar} handleClick={handleClick} toggleNavButton={toggleNavButton} close={toggleSidebar}/>
+      <Sidebar
+        show={showSidebar}
+        handleClick={handleClick}
+        toggleNavButton={toggleNavButton}
+        close={toggleSidebar}
+      />
     </>
   );
 };
---
+++ Frontend/src/components/header/UserWelcome.js
@@ -0,0 +1,39 @@
+import "./userWelcome.css";
+
+const UserWelcome = ({ handleLogout }) => {
+  return (
+    <div className="user-loggedIn-main-container">
+      <div className="user-avatar-container">
+        <h2>
+          {`
+                ${JSON.parse(localStorage.getItem("user")).username.slice(
+                  0,
+                  1
+                )}${JSON.parse(localStorage.getItem("user")).lastname.slice(
+            0,
+            1
+          )}
+                `}
+        </h2>
+      </div>
+      <div className="user-welcome-container">
+        <li type="none">
+          <ul>
+            <p>Hola, </p>
+          </ul>
+          <ul className="user-welcome-username">
+            <p>{`
+                    ${JSON.parse(localStorage.getItem("user")).username}
+                    ${JSON.parse(localStorage.getItem("user")).lastname}        
+                    `}</p>
+          </ul>
+        </li>
+        <div className="user-logout-container" onClick={handleLogout}>
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserWelcome;
