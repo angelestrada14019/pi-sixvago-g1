e4b3651000df93d0d0085866d67f596f2310bee9
GabsFranke <frankegabriel@gmail.com>
2022-05-26 12:21:44 -0300

    añadi stars y rating en las cards

--- Frontend/src/components/body/Card.js
+++ Frontend/src/components/body/Card.js
@@ -1,10 +1,23 @@
 const Card = ({ data }) => {
   return (
-    <div className="card" >
+    <div className="card">
       <img className="card-img" src={`${data.img}`} alt={data.title} />
       <div className="card-body">
         {/* categoría del producto, el nombre, ubicación, la descripción. Y un botón */}
-        <p className="card-category">{`${data.category.toUpperCase()}`}</p>
+
+        <p className="card-category">
+          {`${data.category.toUpperCase()}`}
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+        </p>
+        <div className="card-rating">
+          <h2>8</h2>
+          <p>Muy bueno</p>
+        </div>
+
         <h2 className="card-title">{`${data.title}`}</h2>
         <p className="card-location">{`${data.location}`}</p>
         <p className="card-description">{`${data.description
