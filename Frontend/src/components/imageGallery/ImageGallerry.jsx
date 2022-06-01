6bc6569e342881427c6fff2b48fcbac6e58cba9f
angelestrada14019 <rangel1998.rt@gmail.com>
2022-06-01 12:23:30 -0500

    cambio automatico y que sea en intervalos de 2 segundos

--- Frontend/src/components/imageGallery/ImageGallerry.jsx
+++ Frontend/src/components/imageGallery/ImageGallerry.jsx
@@ -17,19 +17,19 @@ const images = [
     thumbnail: "https://picsum.photos/id/1019/250/150/",
   },
 ];
-const styleBox = {    
-    width: 500,
-    height: 500,
-    // position: "absolute",
-    // top: "50%",
-    // left: "50%",
-}
+const styleBox = {
+  width: 500,
+  height: 500,
+  // position: "absolute",
+  // top: "50%",
+  // left: "50%",
+};
 const styleModal = {
-    // position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-}
+  // position: "relative",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 const ImageGallerry = () => {
   const [open, setOpen] = useState(false);
@@ -38,7 +38,14 @@ const ImageGallerry = () => {
       <button onClick={() => setOpen(true)}>Open modal</button>
       <Modal sx={styleModal} open={open} onClose={() => setOpen(false)}>
         <Box sx={styleBox}>
-            <ImageGallery items={images} />
+          <ImageGallery
+            autoPlay={true}
+            items={images}
+            showPlayButton={false}
+            showFullscreenButton={false}
+            slideDuration={450}
+            slideInterval={3000}
+          />
         </Box>
       </Modal>
     </div>
