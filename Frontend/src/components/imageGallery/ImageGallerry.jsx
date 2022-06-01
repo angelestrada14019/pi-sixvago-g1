86ed75df1fd9a98199ac871f34617b0e4fdcb5bf
angelestrada14019 <rangel1998.rt@gmail.com>
2022-06-01 16:17:20 -0500

    se agrego par version tablet y phone, boton par cerrar el modal, iconos de compartir y me gusta

--- Frontend/src/components/imageGallery/ImageGallerry.jsx
+++ Frontend/src/components/imageGallery/ImageGallerry.jsx
@@ -3,26 +3,51 @@ import React, { useState } from "react";
 import "./imageGallery.css";
 import Modal from "@mui/material/Modal";
 import { Box } from "@mui/system";
+import CloseIcon from '@mui/icons-material/Close';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 const images = [
   {
+    id: 0,
     original: "https://picsum.photos/id/1018/1000/600/",
     thumbnail: "https://picsum.photos/id/1018/250/150/",
   },
   {
+    id: 1,
     original: "https://picsum.photos/id/1015/1000/600/",
     thumbnail: "https://picsum.photos/id/1015/250/150/",
   },
   {
+    id: 2,
     original: "https://picsum.photos/id/1019/1000/600/",
     thumbnail: "https://picsum.photos/id/1019/250/150/",
   },
+  {
+    id: 3,
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+  {
+    id: 4,
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+  {
+    id: 5,
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+  {
+    id: 6,
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
 ];
 const styleBox = {
-  width: 900,
-  height: 700,
+    width: "60%",
+    height: "60%",
 };
 const styleModal = {
-  // position: "relative",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
@@ -31,20 +56,63 @@ const styleModal = {
 const ImageGallerry = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="image_gallery">
-      <button onClick={() => setOpen(true)}>Open modal</button>
-      <Modal sx={styleModal} open={open} onClose={() => setOpen(false)}>
-        <Box sx={styleBox}>
-          <ImageGallery
-            autoPlay={true}
-            items={images}
-            showPlayButton={false}
-            showFullscreenButton={false}
-            slideDuration={450}
-            slideInterval={3000}
-          />
-        </Box>
-      </Modal>
+    <div className="image_gallery_container">
+        <div className="icon_social">
+        <ShareOutlinedIcon/>
+        <FavoriteBorderIcon/>
+        </div>
+      <div className="gallery">
+        {images.slice(0,5).map((image) => (
+          <div className="gallery-image" key={image.id}>
+            <img
+              src={image.original}
+              alt={image.original}
+              style={{ width: "100%" }}
+            />
+          </div>
+        ))}
+        <p className="gallery-button" onClick={() => setOpen(true)}>
+          Ver más
+        </p>
+      </div>
+      <div className="modal_container">
+        <Modal sx={styleModal} open={open} onClose={() => setOpen(false)}>
+          <Box className="image_Container" sx={styleBox}>
+              <div className="close_gallerry_image-button" onClick={() => setOpen(false)}>
+                <CloseIcon />
+              </div>
+            <ImageGallery
+              autoPlay={true}
+              items={images}
+              showPlayButton={false}
+              showFullscreenButton={false}
+              slideDuration={450}
+              slideInterval={3000}
+              showIndex={false}
+              
+            />
+          </Box>
+        </Modal>
+      </div>
+      <div className="container-gallery_image">
+      <div className="icon_social">
+        <ShareOutlinedIcon/>
+        <FavoriteBorderIcon/>
+        </div>
+      <ImageGallery
+              autoPlay={true}
+              items={images}
+              showPlayButton={false}
+              showFullscreenButton={false}
+              slideDuration={450}
+              slideInterval={3000}
+              showIndex={true}
+              showThumbnails={false}
+              showNav={false}
+              
+            />
+      </div>
+      
     </div>
   );
 };
