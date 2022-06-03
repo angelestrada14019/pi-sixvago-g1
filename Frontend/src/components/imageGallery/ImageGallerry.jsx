299e8fa62ef3914f3ecc06df27edf3a028b2642c
angelestrada14019 <rangel1998.rt@gmail.com>
2022-06-03 12:37:43 -0500

    se agrego la carpeta utils, para los fetch, se cambiaron los iconos en imageGallery por los de font awesome

--- Frontend/src/components/imageGallery/ImageGallerry.jsx
+++ Frontend/src/components/imageGallery/ImageGallerry.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import "./imageGallery.css";
 import Modal from "@mui/material/Modal";
 import { Box } from "@mui/system";
-import CloseIcon from '@mui/icons-material/Close';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import CloseIcon from "@mui/icons-material/Close";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 const images = [
   {
     id: 0,
@@ -44,8 +44,8 @@ const images = [
   },
 ];
 const styleBox = {
-    width: "60%",
-    height: "60%",
+  width: "60%",
+  height: "60%",
 };
 const styleModal = {
   display: "flex",
@@ -57,12 +57,12 @@ const ImageGallerry = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="image_gallery_container">
-        <div className="icon_social">
-        <ShareOutlinedIcon/>
-        <FavoriteBorderIcon/>
-        </div>
+      <div className="icon_social">
+        <i class="fa-solid fa-share-nodes"></i>
+        <i class="fa-regular fa-heart"></i>
+      </div>
       <div className="gallery">
-        {images.slice(0,5).map((image) => (
+        {images.slice(0, 5).map((image) => (
           <div className="gallery-image" key={image.id}>
             <img
               src={image.original}
@@ -78,9 +78,12 @@ const ImageGallerry = () => {
       <div className="modal_container">
         <Modal sx={styleModal} open={open} onClose={() => setOpen(false)}>
           <Box className="image_Container" sx={styleBox}>
-              <div className="close_gallerry_image-button" onClick={() => setOpen(false)}>
-                <CloseIcon />
-              </div>
+            <div
+              className="close_gallerry_image-button"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon />
+            </div>
             <ImageGallery
               autoPlay={true}
               items={images}
@@ -89,30 +92,28 @@ const ImageGallerry = () => {
               slideDuration={450}
               slideInterval={3000}
               showIndex={false}
-              
             />
           </Box>
         </Modal>
       </div>
       <div className="container-gallery_image">
-      <div className="icon_social">
-        <ShareOutlinedIcon/>
-        <FavoriteBorderIcon/>
+        <div className="icon_social">
+          <i class="fa-solid fa-share-nodes"></i>
+          <i class="fa-regular fa-heart"></i>
         </div>
-      <ImageGallery
-              autoPlay={true}
-              items={images}
-              showPlayButton={false}
-              showFullscreenButton={false}
-              slideDuration={450}
-              slideInterval={3000}
-              showIndex={true}
-              showThumbnails={false}
-              showNav={false}
-              
-            />
+
+        <ImageGallery
+          autoPlay={true}
+          items={images}
+          showPlayButton={false}
+          showFullscreenButton={false}
+          slideDuration={450}
+          slideInterval={3000}
+          showIndex={true}
+          showThumbnails={false}
+          showNav={false}
+        />
       </div>
-      
     </div>
   );
 };
---
+++ Frontend/src/utils/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    api:{
+        baseURL:"https://back-project-hospital.herokuapp.com"
+    }
+}
