39c5f5eb57f2eedc0edf68a88ee646ca72d30784
angelestrada14019 <rangel1998.rt@gmail.com>
2022-06-08 11:09:54 -0500

    agregando configuracion para poder correr en la ec2

--- backend/base de datos/DumpProyectoIntegrador/proyecto_integrador.sql
+++ backend/base de datos/DumpProyectoIntegrador/proyecto_integrador.sql
@@ -1,145 +1,190 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proyecto_integrador
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema proyecto_integrador
--- -----------------------------------------------------
+--
+-- Table structure for table `booking_sequence`
+--
 
--- -----------------------------------------------------
--- Schema proyecto_integrador
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `proyecto_integrador` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `proyecto_integrador` ;
+DROP TABLE IF EXISTS `booking_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`booking_sequence`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`booking_sequence` (
-  `next_val` BIGINT NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Dumping data for table `booking_sequence`
+--
 
+--
+-- Table structure for table `caracteristicas`
+--
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`caracteristicas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`caracteristicas` (
-  `caracteristicas_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `icono` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`caracteristicas_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `caracteristicas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `caracteristicas` (
+  `caracteristicas_id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `icono` varchar(255) NOT NULL,
+  PRIMARY KEY (`caracteristicas_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`categorias`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`categorias` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NULL DEFAULT NULL,
-  `titulo` VARCHAR(255) NULL DEFAULT NULL,
-  `url_imagen` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Dumping data for table `caracteristicas`
+--
 
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`ciudades`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`ciudades` (
-  `ciudades_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(255) NOT NULL,
-  `pais` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ciudades_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `url_imagen` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
 
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`productos` (
-  `productos_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NOT NULL,
-  `descripcion` VARCHAR(255) NOT NULL,
-  `direccion` VARCHAR(255) NOT NULL,
-  `politicas_servicios` VARCHAR(255) NULL DEFAULT NULL,
-  `ciudades_id` BIGINT NOT NULL,
-  `categorias_id` BIGINT NOT NULL,
-  PRIMARY KEY (`productos_id`),
-  INDEX `fk_productos_ciudades1_idx` (`ciudades_id` ASC) VISIBLE,
-  INDEX `fk_productos_categorias1_idx` (`categorias_id` ASC) VISIBLE,
-  CONSTRAINT `fk_productos_categorias1`
-    FOREIGN KEY (`categorias_id`)
-    REFERENCES `proyecto_integrador`.`categorias` (`id`),
-  CONSTRAINT `fk_productos_ciudades1`
-    FOREIGN KEY (`ciudades_id`)
-    REFERENCES `proyecto_integrador`.`ciudades` (`ciudades_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Table structure for table `ciudades`
+--
+
+DROP TABLE IF EXISTS `ciudades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ciudades` (
+  `ciudades_id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `pais` varchar(255) NOT NULL,
+  PRIMARY KEY (`ciudades_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ciudades`
+--
 
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`imagenes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`imagenes` (
-  `imagenes_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NOT NULL,
-  `url` VARCHAR(2048) NOT NULL,
-  `productos_id` BIGINT NOT NULL,
+--
+-- Table structure for table `imagenes`
+--
+
+DROP TABLE IF EXISTS `imagenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `imagenes` (
+  `imagenes_id` bigint NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  `productos_id` bigint NOT NULL,
   PRIMARY KEY (`imagenes_id`),
-  INDEX `fk_imagenes_productos1_idx` (`productos_id` ASC) VISIBLE,
-  CONSTRAINT `fk_imagenes_productos1`
-    FOREIGN KEY (`productos_id`)
-    REFERENCES `proyecto_integrador`.`productos` (`productos_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_imagenes_productos1_idx` (`productos_id`),
+  CONSTRAINT `fk_imagenes_productos1` FOREIGN KEY (`productos_id`) REFERENCES `productos` (`productos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagenes`
+--
+
+LOCK TABLES `imagenes` WRITE;
+/*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paciente_sequence`
+--
+
+--
+-- Dumping data for table `paciente_sequence`
+--
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `productos_id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `politicas_servicios` varchar(255) DEFAULT NULL,
+  `ciudades_id` bigint NOT NULL,
+  `categorias_id` bigint NOT NULL,
+  PRIMARY KEY (`productos_id`),
+  KEY `fk_productos_ciudades1_idx` (`ciudades_id`),
+  KEY `fk_productos_categorias1_idx` (`categorias_id`),
+  CONSTRAINT `fk_productos_categorias1` FOREIGN KEY (`categorias_id`) REFERENCES `categorias` (`id`),
+  CONSTRAINT `fk_productos_ciudades1` FOREIGN KEY (`ciudades_id`) REFERENCES `ciudades` (`ciudades_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
 
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`paciente_sequence`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`paciente_sequence` (
-  `next_val` BIGINT NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
 
+--
+-- Table structure for table `productos_caracteristicas`
+--
 
--- -----------------------------------------------------
--- Table `proyecto_integrador`.`productos_caracteristicas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto_integrador`.`productos_caracteristicas` (
-  `caracteristicas_id` BIGINT NOT NULL,
-  `productos_id` BIGINT NOT NULL,
-  INDEX `fk_productos_caracteristicas_caracteristicas1_idx` (`caracteristicas_id` ASC) VISIBLE,
-  INDEX `fk_productos_caracteristicas_productos1_idx` (`productos_id` ASC) VISIBLE,
-  CONSTRAINT `fk_productos_caracteristicas_caracteristicas1`
-    FOREIGN KEY (`caracteristicas_id`)
-    REFERENCES `proyecto_integrador`.`caracteristicas` (`caracteristicas_id`),
-  CONSTRAINT `fk_productos_caracteristicas_productos1`
-    FOREIGN KEY (`productos_id`)
-    REFERENCES `proyecto_integrador`.`productos` (`productos_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `productos_caracteristicas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos_caracteristicas` (
+  `caracteristicas_id` bigint NOT NULL,
+  `productos_id` bigint NOT NULL,
+  KEY `fk_productos_caracteristicas_caracteristicas1_idx` (`caracteristicas_id`),
+  KEY `fk_productos_caracteristicas_productos1_idx` (`productos_id`),
+  CONSTRAINT `fk_productos_caracteristicas_caracteristicas1` FOREIGN KEY (`caracteristicas_id`) REFERENCES `caracteristicas` (`caracteristicas_id`),
+  CONSTRAINT `fk_productos_caracteristicas_productos1` FOREIGN KEY (`productos_id`) REFERENCES `productos` (`productos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `productos_caracteristicas`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-08 11:44:27
