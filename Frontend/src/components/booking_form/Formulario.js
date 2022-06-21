3984146ee8812b6c93f3f35e4723c4ecec91d6e8
Jenny Vargas <jennypatriciavn@gmail.com>
2022-06-20 22:27:57 -0500

    formulario reserva

---
+++ Frontend/src/components/booking_form/Formulario.js
@@ -0,0 +1,122 @@
+
+import React, { useEffect, useState } from 'react';
+import { Formik } from 'formik';
+import "./form.css"
+import ApiCall from '../../utils/ApiCall';
+
+
+const Formulario = () => {
+const user = JSON.parse(localStorage.getItem("user")); 
+/* const [user, setUser] = useState('');
+
+  useEffect(() => {
+    getUser();
+
+  }, [user]);
+  const getUser = async () => {
+    const userSaved = await ApiCall.invokeGET("/users/" + user.id);
+  setUser(userSaved);
+  }
+ */
+  return (
+    
+    <>
+    <h2 className="section-h2">Completá tus datos</h2>
+      <Formik
+        initialValues={{
+          nombre: '',
+          apellido: '',
+          email: '',
+          ciudad: '',
+          hora: '',
+        }}
+        validate={(valores) => {
+          let errores = {};
+          if (!valores.ciudad) {
+            console.log('el nombre es obligatorio');
+          }
+        }}
+        onSubmit={(valores) => {
+          console.log('Formulario enviado', valores);
+        }}
+      >
+        {({ values, errors, handleSubmit, handleChange, handleBlur }) => (
+          <form className="formulario" onSubmit={handleSubmit}>
+
+            <div id="form-group"class="clearfix" >
+            <div class="form-row">
+            <div className="column-half">
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                disabled
+                id="nombre"
+                name="nombre"
+                placeholder="Ingrese  su nombre"
+                value={user.username}
+                onChange={handleChange}
+                onBlur={handleBlur} />
+            </div>
+
+            
+
+            <div className="column-half">
+              <label htmlFor="apellido">Apellido</label>
+              <input
+                type="text"
+                disabled
+                id="apellido"
+                name="apellido"
+                placeholder="Ingrese  su apellido"
+                value={user.lastname}
+                onChange={handleChange}
+                onBlur={handleBlur} />
+
+            </div>
+
+            <div className="column-half">
+              <label htmlFor="correo">Email</label>
+              <input
+                type="email"
+                disabled
+                id="correo"
+                name="correo"
+                placeholder="Ingrese correo Electronico"
+                value={user.email}
+                onChange={handleChange}
+                onBlur={handleBlur} />
+            </div>
+
+          
+            <div className="column-half">
+              <label htmlFor="ciudad">Ciudad</label>
+              <input
+                type="text"
+                required
+                id="ciudad"
+                name="ciudad"
+                placeholder="Ingrese su ciudad"
+                value={values.ciudad}
+                onChange={handleChange}
+                onBlur={handleBlur} />
+                
+
+            </div>
+            </div>
+            </div>
+
+            
+
+           
+          </form>
+        )}
+      </Formik>
+
+      <div>
+      <h2 className="section-h2">Seleccioná tu fecha de reserva</h2>
+      </div>
+     
+    </>
+  );
+}
+export default Formulario;
