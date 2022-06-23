54935e942d4c6418af456d3557c16c6b6e3480de
GabsFranke <frankegabriel@gmail.com>
2022-06-23 16:50:05 -0300

    arreglo alertas

--- Frontend/src/utils/CustomAlert.js
+++ Frontend/src/utils/CustomAlert.js
@@ -1,45 +1,37 @@
-// import { Alert, Snackbar } from "@mui/material";
-// import { useState } from "react";
+import { Alert, Snackbar } from "@mui/material";
+import { useState } from "react";
 
-// const CustomAlert = ({
-//   open,
-//   text,
-//   snackSx,
-//   anchor,
-//   severity,
-//   variant,
-//   alertSx,
-//   error,
-//   setError,
-//   alert,
-//   setAlert,
-// }) => {
-//   const [bool, setBool] = useState(open || error || alert || false);
-//   const handleClose = (event, reason) => {
-//     if (reason === "clickaway") {
-//       setBool(false);
-//       setAlert(false);
-//       setError(false);
-//       return;
-//     }
-//     setBool(false);
-//     setAlert(false);
-//     setError(false);
-//   };
+const CustomAlert = ({
+  open,
+  text,
+  snackSx,
+  anchor,
+  severity,
+  variant,
+  alertSx,
+}) => {
+  const [bool, setBool] = useState(open);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      setBool(false);
+      return;
+    }
+    setBool(false);
+  };
 
-//   return (
-//     <Snackbar
-//       sx={snackSx}
-//       open={bool}
-//       autoHideDuration={6000}
-//       onClose={handleClose}
-//       anchorOrigin={anchor}
-//     >
-//       <Alert severity={severity} variant={variant || "outlined"} sx={alertSx}>
-//         {text}
-//       </Alert>
-//     </Snackbar>
-//   );
-// };
+  return (
+    <Snackbar
+      sx={snackSx}
+      open={bool}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={anchor}
+    >
+      <Alert severity={severity} variant={variant || "outlined"} sx={alertSx}>
+        {text}
+      </Alert>
+    </Snackbar>
+  );
+};
 
-// export default CustomAlert;
+export default CustomAlert;
