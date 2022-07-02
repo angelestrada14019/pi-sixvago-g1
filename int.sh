5456dca5a8fad6fd7d362d326ed19416c05d53ee
angelestrada14019 <rangel1998.rt@gmail.com>
2022-07-02 17:29:47 -0500

    cambiando el init.sh

--- int.sh
+++ int.sh
@@ -2,10 +2,10 @@ echo "Antes configurar la variable de entorno GITLAB_TOKEN:"
 echo "export GITLAB_TOKEN=<SU_TOKEN_DE_GITLAB>"
 
 terraform init \
-    -backend-config="address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws" \
-    -backend-config="lock_address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws/lock" \
-    -backend-config="unlock_address=https://gl.deitech.online/api/v4/projects/222/terraform/state/gitlab-terraform-aws/lock" \
-    -backend-config="username=<username>" \
+    -backend-config="address=https://gl.deitech.online/api/v4/projects/411/terraform/state/grupo-01" \
+    -backend-config="lock_address=https://gl.deitech.online/api/v4/projects/411/terraform/state/grupo-01/lock" \
+    -backend-config="unlock_address=https://gl.deitech.online/api/v4/projects/411/terraform/state/grupo-01/lock" \
+    -backend-config="username=angelestrada14" \
     -backend-config="password=$GITLAB_TOKEN" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
