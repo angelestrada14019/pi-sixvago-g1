06333308c73a64f87306d99904e463a1f88927bb
angelestrada14019 <rangel1998.rt@gmail.com>
2022-07-05 13:15:56 -0500

    politicas del front

--- Frontend/src/components/product/Politicas.js
+++ Frontend/src/components/product/Politicas.js
@@ -6,8 +6,8 @@ const Politicas = ({ producto }) => {
   useEffect(() => {
     getTiposDePoliticas();
   }, []);
-  const getTiposDePoliticas = () => {
-    const response = ApiCall.invokeGET(`/tipoDePoliticas`);
+  const getTiposDePoliticas = async() => {
+    const response =await ApiCall.invokeGET(`/tipoDePoliticas`);
     try {
       if (response.ok) {
         setTiposPolitica(response.body);
@@ -20,47 +20,23 @@ const Politicas = ({ producto }) => {
     <div className="politicas">
       <h2 className="politicas-title">Que tenes que saber</h2>
       <hr></hr>
-      {producto.politicas?.length > 0 ? (
+    
         <div className="politicas-container">
           {tiposPolitica?.map((tipoPolitica, i) => (
-            <div className="politicasSection">
+            <div key={`${tipoPolitica.nombre}`} className="politicasSection">
               <h3 className="subsection-title">{tipoPolitica.nombre}</h3>
               <ul>
                 {producto.politicas?.map(
                   (politica, i) =>
                     politica.tipoDePolitica.id === tipoPolitica.id && (
-                      <li className="lista">{}</li>
+                      <li key={`${tipoPolitica.nombre}${politica.descripcion}`} className="lista">{politica.descripcion}</li>
                     )
-                )}
-                {producto.politicas.tipoDePolitica === 1 && (
-                  <li className="lista">{}</li>
                 )}
               </ul>
             </div>
           ))}
-
-          <div className="politicasSection">
-            <h3 className="subsection-title">Seguridad</h3>
-            <ul>
-              <li className="lista">
-                {producto.politicas_servicio !== undefined &&
-                  producto.politicas_servicio.split(",")[6]}
-              </li>
-            </ul>
-          </div>
-          <div className="politicasSection">
-            <h3 className="subsection-title">Cancelacion</h3>
-            <ul>
-              <li className="lista">
-                {producto.politicas_servicio !== undefined &&
-                  producto.politicas_servicio.split(",")[11]}
-              </li>
-            </ul>
-          </div>
         </div>
-      ) : (
-        <p>No hay politicas</p>
-      )}
+     
     </div>
   );
 };
