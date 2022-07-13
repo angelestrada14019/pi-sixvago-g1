4e9ad3d15f2ebd352b1038be322dba86543c1155
angelestrada14019 <rangel1998.rt@gmail.com>
2022-07-13 07:08:26 +0000

    Update deploy_app.sh

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
+set -x 
+
+DATA='{ "updates": [ { "type": "web", "docker_image":"'
+DATA="$DATA$IMAGE_ID"
+DATA=$DATA'"}, { "type": "worker", "docker_image":"'
+DATA=$DATA'" } ] }'
+
 curl --netrc -X PATCH https://api.heroku.com/apps/back-pi-sixvago-g1-c2/formation \
-  -d '{
-  "updates": [
-    {
-      "type": "web",
-      "docker_image": "${IMAGE_ID}"
-    },
-    {
-      "type": "worker",
-      "docker_image": "{$IMAGE_ID}"
-    }
-  ]
-}' \
+  -d "$DATA" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
   -H "Authorization: Bearer ${HEROKU_TOKEN}"
