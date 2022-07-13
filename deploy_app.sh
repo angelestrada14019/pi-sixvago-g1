5fbfefa34c3c7d5ce3038d2a83c7a5135beb62ea
angelestrada14019 <rangel1998.rt@gmail.com>
2022-07-13 06:12:39 +0000

    Update deploy_app.sh

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -x 
-
+echo "1"
 DATA='{ "updates": [ { "type": "web", "docker_image":"'
 DATA="$DATA$IMAGE_ID"
 DATA=$DATA'" } ] }'
+echo "2"
 
 curl -X PATCH https://api.heroku.com/apps/back-pi-sixvago-g1-c2/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data "$DATA" --fail
+
+echo "3"
