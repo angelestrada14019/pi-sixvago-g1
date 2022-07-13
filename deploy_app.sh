5beb2fb364ab78f7315c508b2d06f36af77e0e2d
angelestrada14019 <rangel1998.rt@gmail.com>
2022-07-13 06:00:02 +0000

    Update deploy_app.sh, .gitlab-ci.yml

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,15 +1,8 @@
-curl --netrc -X PATCH https://api.heroku.com/apps/back-pi-sixvago-g1-c2/formation \
-  -d '{
-  "updates": [
-    {
-      "type": "web",
-      "docker_image": "registry.heroku.com/back-pi-sixvago-g1-c2:latest"
-    },
-    {
-      "type": "worker",
-      "docker_image": "registry.heroku.com/back-pi-sixvago-g1-c2:latest"
-    }
-  ]
-}' \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+#!/bin/sh
+set -x 
+
+DATA='{ "updates": [ { "type": "web", "docker_image":"'
+DATA="$DATA$IMAGE_ID"
+DATA=$DATA'" } ] }'
+
+curl -X PATCH https://api.heroku.com/apps/back-pi-sixvago-g1-c2/formation --header "Content-Type: application/json" --header "Accept: application/vnd.heroku+json; version=3.docker-releases" --header "Authorization: Bearer ${HEROKU_TOKEN}" --data "$DATA" --fail
